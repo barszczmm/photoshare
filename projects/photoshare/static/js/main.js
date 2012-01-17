8bb031efa69d79106247f1d3c3881a48d48030c5
Maciej 'barszcz' Marczewski <maciej@marczewski.net.pl>
2012-01-17 01:32:59 +0100

    added static files for project

---
+++ projects/photoshare/static/js/main.js
@@ -0,0 +1,53 @@
+$('.errorlist').each(
+	function() {
+		if ($('textarea', $(this).next()).length) {
+			$(this).animate({width: 320}, 500);
+		} else {
+			$(this).animate({width: 700}, 1000);
+		}
+	}
+);
+
+
+$(document).ready(function() {
+
+	//
+	$('input[type="submit"]').hover(
+		function() {
+			$(this).fadeTo(200, 0.5);
+		},
+		function() {
+			$(this).fadeTo(200, 1);
+		}
+	);
+
+
+	$('#user_menu .authenticated .account').hover(
+		function() {
+			$('ul', this).slideDown();
+		},
+		function() {
+			$('ul', this).slideUp();
+		}
+	).find('ul').css('opacity', 0.9);
+
+	
+	$('.tabs').each(
+		function() {
+			var $tabs = $(this);
+			$('.tab-title', $tabs).click(
+				function() {
+					var $clicked = $(this);
+					if (!$clicked.hasClass('active')) {
+						$clicked.addClass('active').siblings().removeClass('active');
+						$('.tab-content:eq('+$clicked.index()+')', $tabs).addClass('active').siblings().removeClass('active');
+					}
+				}
+			);
+		}
+	);
+	if (window.location.hash != '') {
+		$('.tabs .tab-title a[href='+window.location.hash+']').parent().click();
+	}
+
+});
