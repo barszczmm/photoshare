6a10bd5c2ae74dc7ca8f56c4ac67da2814e42b9c
Maciej 'barszcz' Marczewski <maciej@marczewski.net.pl>
2012-01-17 12:47:28 +0100

    added view for comments for user profile

--- apps/profiles/views.py
+++ apps/profiles/views.py
@@ -1,1 +1,22 @@
-# Create your views here.
+from django.shortcuts import render_to_response, get_object_or_404
+from django.http import Http404
+from django.template import RequestContext
+from django.contrib.auth.models import User
+
+from models import Profile
+
+
+def profile_comments(request, username):
+    """
+    List comments for user profile.
+    """
+    user = get_object_or_404(User, username=username)
+    try:
+        profile = user.get_profile()
+    except Profile.DoesNotExist:
+        raise Http404
+
+    return render_to_response('profiles/profile_comments.html',
+                              {'profile': profile},
+                              context_instance=RequestContext(request))
+
