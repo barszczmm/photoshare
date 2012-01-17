e3b0fe33137612de67530db6f3f145719fefd5e4
Maciej 'barszcz' Marczewski <maciej@marczewski.net.pl>
2012-01-17 01:31:25 +0100

    added urls for admin, accounts, messages, comments

--- projects/photoshare/photoshare/urls.py
+++ projects/photoshare/photoshare/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -13,5 +13,12 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^accounts/', include('userena.urls')),
+
+    url(r'^messages/', include('userena.contrib.umessages.urls')),
+
+    url(r'^comments/', include('django.contrib.comments.urls')),
+
 )
