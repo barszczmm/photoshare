01253e94145a9ba8affbfb5a2f6c4a924de5fe82
Maciej 'barszcz' Marczewski <maciej@marczewski.net.pl>
2012-01-25 12:06:21 +0100

    prepared some urls for AJAX

--- apps/profiles/urls.py
+++ apps/profiles/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls.defaults import *
+#from django.contrib.auth import views as auth_views
+
+#from userena import views as userena_views
 
 urlpatterns = patterns('profiles.views',
 
@@ -14,5 +17,19 @@ urlpatterns = patterns('profiles.views',
         'stop_being_profile_fan',
         name='profile_stop_being_fan'),
 
+    # AJAX vesrions of some userena views
+    #url(r'^signup/ajax/$',
+       #userena_views.signup,
+       #{'template_name': 'userena/signup_form_ajax.html'},
+       #name='userena_signup_ajax'),
+    #url(r'^signin/ajax/$',
+       #userena_views.signin,
+       #{'template_name': 'userena/signin_form_ajax.html'},
+       #name='userena_signin_ajax'),
+    #url(r'^password/reset/ajax/$',
+       #auth_views.password_reset,
+       #{'template_name': 'userena/password_reset_form_ajax.html',
+        #'email_template_name': 'userena/emails/password_reset_message.txt'},
+       #name='userena_password_reset_ajax'),
 )
 
